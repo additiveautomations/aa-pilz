8f5e8ff9c37879479f20df945eeeb98aab491aa1
Tom Noble <tom@additiveautomations.com>
2021-07-06 09:36:25 +0100

    Correctly construct PlanningSceneConstPtr

--- moveit_planners/pilz_industrial_motion_planner/src/trajectory_blender_transition_window.cpp
+++ moveit_planners/pilz_industrial_motion_planner/src/trajectory_blender_transition_window.cpp
@@ -86,9 +86,10 @@ bool pilz_industrial_motion_planner::TrajectoryBlenderTransitionWindow::blend(
   trajectory_msgs::JointTrajectory blend_joint_trajectory;
   moveit_msgs::MoveItErrorCodes error_code;
 
-  const planning_scene::PlanningSceneConstPtr& scene;
+  const moveit::core::RobotModelConstPtr& robot_model = req.first_trajectory->getFirstWayPointPtr()->getRobotModel();
+  const planning_scene::PlanningSceneConstPtr& scene = &planning_scene::PlanningScene(robot_model);
 
-  if (!generateJointTrajectory(scene, req.first_trajectory->getFirstWayPointPtr()->getRobotModel(),
+  if (!generateJointTrajectory(scene, robot_model,
                                limits_.getJointLimitContainer(), blend_trajectory_cartesian, req.group_name,
                                req.link_name, initial_joint_position, initial_joint_velocity, blend_joint_trajectory,
                                error_code, true))
